d33230474a1b57c029c270791696a46a044aefd1
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-05 22:11:56 -0400

    Add ANOTHER gina face!

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,13 @@ import 'bulma/css/bulma.css'
 
 function App() {
 	return (
-		<section class='section'>
+		<section class='hero is-dark is-bold is-fullheight'>
 			<div class='container'>
 				<h1 class='title'>
 					Gina Raimondo: "RONA stonks are up!"
 				</h1>
 				<p class='subtitle'>
-
+					According to all known laws of aviation, there is no reason a bee should be able to fly.
 				</p>
 				<h1 class='title is-1'>3,000</h1>
 				<img src={gina} />
