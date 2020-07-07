6288ff49a6e5d311dd898c2720ec1be24b37121a
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-06 23:41:58 -0400

    stylish updates

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,8 @@ import { calculateTotalNewCases } from './calculateTotalNewCases';
 import { fetchCovidCases as getCovidCasesWith } from './fetchCovidCases';
 
 // TODO: Why is there an error?
-import gina from './assets/two.png';
+// import gina from './assets/two.png';
 import 'bulma/css/bulma.css'
-
 
 function App() {
 	const [cases, setCases] = useState(0);
@@ -18,22 +17,22 @@ function App() {
 	});
 
 	return (
-		<section class='hero is-dark is-bold is-fullheight'>
+		<section class='hero section is-dark is-fullheight'>
 			<div class='container'>
-				<h1 class='title'>
-					RI COVID-19 CASES SINCE 4TH OF JULY WEEKEND
-				</h1>
+				<h3 class='title is-3'>
+					Rhode Island's COVID-19 cases since 4th of July weekend
+				</h3>
 
 				<p class='subtitle'>
-					This is the <b>total</b> number of new cases in RI starting once the <b>July 4th weekend</b> (July 3 - July 5) finishes.
+					This is the <b>total</b> number of <b>new</b> cases in RI starting once the <b>July 4th weekend</b> (July 3 - July 5) finishes.
 				</p>
 
-				<h1 class='title is-1'>
+				<h1 class='title is-1 has-text-centered has-text-danger' style={{ fontSize: '34vh' }}>
 					{cases}
 				</h1>
 
-				<img src={gina} />
-				<p class='subtitle is-4'>
+				{/* TODO: Move this to .scss file */}
+				<p class='bottom-text subtitle is-4' style={{ position: 'absolute', bottom: '0' }}>
 					<i>"Now's a great time to vacation in Rhode Island, and even visit your second home!"</i> - <b>Gina Romundus</b>, 2020
 				</p>
 			</div>
