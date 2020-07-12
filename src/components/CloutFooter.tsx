716133fce8bfdc786201ee71df2cea27431f13c7
Andrew Colannino <akc8012@gmail.com>
2020-07-12 10:30:44 -0400

    clout style is now finished, completing the Clout Cycle(TM)

--- src/components/CloutFooter.tsx
+++ src/components/CloutFooter.tsx
@@ -2,14 +2,12 @@ import { h } from 'preact';
 
 export function CloutFooter() {
 	return (
-		<footer class='footer has-background-dark'>
+		<footer class='footer has-background-dark' style={{ padding: '5rem 1.5rem 0.7rem' }}>
 			<div class='content has-text-light is-small has-text-centered'>
-				<p>
+				<p style={{ color: 'rgba(255, 255, 255, 0.4)' }}>
 					Made by <CloutLink href='https://acolannino.io'>acolannino</CloutLink>, <b>u/whalecat4</b>,
 					and <CloutLink href='https://github.com/dillonfagan'>dillonfagan</CloutLink>, with help from friends.
-				</p>
 
-				<p>
 					This app is <CloutLink href='https://en.wikipedia.org/wiki/Free_software'>free software</CloutLink>,
 					and its source is available on <CloutLink href='https://github.com/akc8012/gina-rona-app'>GitHub</CloutLink>.
 				</p>
