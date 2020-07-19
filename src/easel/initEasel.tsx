9864a77ffe1fb0e932fb2cd5deb1ed81a8c716b0
Andrew Colannino <akcolannino@email.neit.edu>
2020-07-19 19:05:11 -0400

    gina face bad animation

--- src/easel/initEasel.tsx
+++ src/easel/initEasel.tsx
@@ -4,13 +4,18 @@ import ginaImage from '../assets/three.png';
 
 
 let stage: any = undefined;
+
 const ginaFace = new createjs.Bitmap(ginaImage);
+const ginaScaleFactor = 0.2;
+const ginaFaceHeight = ginaFace.image.height * ginaScaleFactor;
 
 export function initEasel() {
 	stage = createStage();
 
-	ginaFace.x = stage.canvas.width;
-	ginaFace.y = 40;
+	ginaFace.x = 0;
+	ginaFace.y = stage.canvas.height;
+	ginaFace.scale = ginaScaleFactor;
+	ginaFace.alpha = 0.88;
 	stage.addChild(ginaFace);
 
 	console.log('my body is ready');
@@ -18,6 +23,13 @@ export function initEasel() {
 
 createjs.Ticker.framerate = 60;
 createjs.Ticker.addEventListener('tick', function () {
-	ginaFace.x -= 0.6;
+	ginaFace.x += 6;
+	ginaFace.y -= 10;
+
+	if (ginaFace.y + ginaFaceHeight < 0) {
+		ginaFace.x = 0;
+		ginaFace.y = stage.canvas.height;
+	}
+
 	stage.update();
 });
